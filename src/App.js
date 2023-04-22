1c9420952aa44626724944c45a36fde90b1e6f43
jaden.1230 <jaden.1230@kakaocorp.com>
2023-04-22 20:10:41 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Container from '@mui/material/Container';
+
+import UploadFile from './component/UploadFile'
+import { Grid } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ripeBanana stack-large">
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid item xs={12} md={4} lg={3}>
+          <h1>Are These bananas ripe?</h1>
+        </Grid>
+        <Grid>
+          <UploadFile />
+        </Grid>
+      </Container>
     </div>
   );
 }
